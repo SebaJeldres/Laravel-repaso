cf543b61bdb300ae2ec5cbee4334ede4ba8a3f3f
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-10-17 18:43:51 -0300

    Agregar controlador Boleta y modelo, junto con migraciones para la tabla boletas y el campo proveedor

---
+++ CrudBoletas/app/Http/Controllers/BoletaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Boleta;
+use Illuminate\Http\Request;
+
+class BoletaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Boleta $boleta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Boleta $boleta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Boleta $boleta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Boleta $boleta)
+    {
+        //
+    }
+}
